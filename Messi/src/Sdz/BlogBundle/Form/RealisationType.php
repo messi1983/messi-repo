0d94f2035d6be2ea9ba039b848906f2b1061d55e
messi <messi_louis@yahoo.fr>
2016-01-03 21:14:08 +0100

    ajout d'un nouveau

---
+++ Messi/src/Sdz/BlogBundle/Form/RealisationType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Sdz\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Sdz\BlogBundle\Constants\Constants;
+
+/**
+ * Realisation Type.
+ * 
+ * @author Messi
+ *
+ */
+class RealisationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+			->add('$application', 'text', array('label' => 'form.field.street.number', 'attr' => array('class' => 'subContent')))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sdz\BlogBundle\Entity\Realisation'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'sdz_blogbundle_realisationtype';
+    }
+}
